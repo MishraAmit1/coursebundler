01b756609361739285e7bf620547eb7130b2a9a7
Amit Mishra <ams@gmail.com>
2023-03-12 15:21:33 +0530

    first commit

---
+++ routes/courseRoutes.js
@@ -0,0 +1,35 @@
+import express from "express";
+import {
+  addLecture,
+  deleteCourse,
+  delteLecture,
+  getCourseLecturess,
+} from "../controllers/courseControllers.js";
+import { createCourse } from "../controllers/courseControllers.js ";
+import { getAllCourses } from "../controllers/courseControllers.js ";
+authorizeSubscribers;
+import {
+  authorizeAdmin,
+  isAuthenticated,
+  authorizeSubscribers,
+} from "../middlewares/authh.js";
+import singleUpload from "../middlewares/multer.js";
+
+const router = express.Router();
+// Get All Courses without lectures
+router.route("/courses").get(getAllCourses);
+// create a new course only admin
+router
+  .route("/createcourse")
+  .post(isAuthenticated, authorizeAdmin, singleUpload, createCourse);
+// .post(isAuthenticated, authorizeAdmin, singleUpload, createCourse);
+//    Add lecture Delete Course    Get Course Details
+router
+  .route("/course/:id")
+  // .get(isAuthenticated, getCourseLecturess)
+  .get(isAuthenticated, authorizeSubscribers, getCourseLecturess)
+  .post(isAuthenticated, authorizeAdmin, singleUpload, addLecture)
+  .delete(isAuthenticated, authorizeAdmin, deleteCourse);
+// Delete Lecture
+router.route("/lecture").delete(isAuthenticated, authorizeAdmin, delteLecture);
+export default router;
